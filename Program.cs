123282b2a8ab48048bb1b0a807e6c2130339e394
Janio Alexandre <janioalexandre@gmail.com>
2024-01-02 22:52:43 -0300

    3 - Criando projetos .NET

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // Hello World!
             Console.WriteLine("Hello World!");
+
+            // Entrada e saída de dados no terminal
+            Console.WriteLine("Digite seu nome:");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome);
         }
     }
 }
