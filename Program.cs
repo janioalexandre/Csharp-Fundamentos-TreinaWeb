02ebe8a3135dc9146357c9ede6e884488a16bd3e
Janio Alexandre <janioalexandre@gmail.com>
2024-01-03 23:58:19 -0300

    4 - Variáveis e tipos de dados - Conversões de tipos

--- Program.cs
+++ Program.cs
@@ -4,13 +4,37 @@
     {
         static void Main(string[] args)
         {
-            // Hello World!
+            /* Hello World!
             Console.WriteLine("Hello World!");
+            */
 
-            // Entrada e saída de dados no terminal
+            /* Entrada e saída de dados no terminal
             Console.WriteLine("Digite seu nome:");
             var nome = Console.ReadLine();
             Console.WriteLine("Olá, " + nome);
+            */
+
+            /* Declaração de variáveis
+            int idade;
+            decimal altura;
+            string nome;
+
+            nome = "Janio Alexandre";
+            string email = "janioalexandre@gmail.com";
+
+            Console.WriteLine(email);
+            */
+            
+            /* Conversões de tipos */ 
+            decimal taxa = 12.98m;
+            DateTime data = DateTime.Now;
+            string taxa2 = taxa.ToString();
+            string data2 = data.ToString();
+
+            string resposta = "12";
+            int idade = int.Parse(resposta);
+            
+            Console.WriteLine(idade);
         }
     }
 }
