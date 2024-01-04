9c3c0dea30ae45305ce60ee886c75adefaa9437a
Janio Alexandre <janioalexandre@gmail.com>
2024-01-04 15:59:21 -0300

    4 - Variáveis e tipos de dados - O tipo String

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@
             Console.WriteLine(email);
             */
             
-            /* Conversões de tipos */ 
+            /* Conversões de tipos 
             decimal taxa = 12.98m;
             DateTime data = DateTime.Now;
             string taxa2 = taxa.ToString();
@@ -33,8 +33,24 @@
 
             string resposta = "12";
             int idade = int.Parse(resposta);
-            
             Console.WriteLine(idade);
+            */ 
+
+            /* O tipo String */
+            var nome = "Janio";
+            var sobrenome = "Alexandre";
+            var nomeCompleto = nome + " " + sobrenome;
+            var nomeCompleto2 = $"{ nome } { sobrenome }.";
+
+            Console.WriteLine(nomeCompleto2.ToUpper());
+            Console.WriteLine(nomeCompleto2.ToLower());   
+            Console.WriteLine(nomeCompleto2.Substring(4));
+            Console.WriteLine(nomeCompleto2.Substring(4, 5));
+            Console.WriteLine(nomeCompleto2.Contains("a"));   
+            Console.WriteLine(nomeCompleto2.StartsWith("J"));   
+            Console.WriteLine(nomeCompleto2.EndsWith("e"));   
+            Console.WriteLine(nomeCompleto2.IndexOf("a"));   
+            Console.WriteLine(nomeCompleto2.LastIndexOf("e"));   
         }
     }
 }
